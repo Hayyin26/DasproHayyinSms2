81cf6df417b8a309093214e96608760df83af0f9
Hayyin26 <hayinmemon@gmail.com>
2024-02-22 04:08:46 +0700

    Initial Commmit

---
+++ Minggu2/Buku25.java
@@ -0,0 +1,42 @@
+package Minggu2;
+
+public class Buku25 {
+    String judul, pengarang;
+    int halaman, stok, harga;
+
+    public static void tampilInformasi() {
+        System.out.println("Judul: " + judul);
+        System.out.println("Pengarang: " + pengarang);
+        System.out.println("Jumlah halaman: " + halaman);
+        System.out.println("Sisa stok: " + stok);
+        System.out.println("Harga: Rp " + harga);
+    }
+
+    void terjual(int jml) {
+        if (stok > 0) {
+            stok -= jml;
+        } else {
+            System.out.println("Tidak ada stok");
+        }
+    }
+
+    void restock(int jml) {
+        stok += jml;
+    }
+
+    void gantiHarga(int hrg) {
+        harga = hrg;
+    }
+
+    public Buku25() {
+
+    }
+
+    public Buku25(String jud, String pg, int hal, int stok, int har) {
+        judul = jud;
+        pengarang = pg;
+        halaman = hal;
+        this.stok = stok;
+        harga = har;
+    }
+}
